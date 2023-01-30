e3857d9f784baa910143f29f49fd12bb06d0a20f
Axel Maldonado <sonic775@outlook.es>
2023-01-29 22:22:06 -0300

    first calculator idea

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-const numberButtons = document.querySelector('#numbers');
-const numberBottomButtons = document.querySelector('#bottom');
+const numberButtons = document.querySelectorAll('.number');
 const currentNumber = document.querySelector('#currentValue');
 const totalValue = document.querySelector('#totalValue');
 
-for (let i = 1; i < 10; i++) {
-    let button = document.createElement('div');
-    button.classList.add('number');
-    button.textContent = `${i}`;
-    button.addEventListener('mousedown', (e) => insertNumber(e));
-    numberButtons.appendChild(button);
-}
-
-let sign = document.createElement('div');
-sign.classList.add('number');
-sign.textContent = '+/-';
-numberBottomButtons.appendChild(sign);
-
-let zero = document.createElement('div');
-zero.classList.add('number');
-zero.textContent = '0';
-numberBottomButtons.appendChild(zero);
-
-let decimal = document.createElement('div');
-decimal.classList.add('number');
-decimal.textContent = '.';
-numberBottomButtons.appendChild(decimal);
-
+let intTotalValue = 0;
+let totalValuePreview = false;
+let isFinalNumber = false;
+numberButtons.forEach((number) => number.addEventListener('click', insertNumber));
+document.querySelector('#sign').addEventListener('click', changeSign);
+document.querySelector('#decimal').addEventListener('click', addDecimal);
+document.querySelectorAll('.operation').forEach((operation) => operation.addEventListener('click', (e) => operate(e.target.textContent)))
 //#region Basic Functions
 function add(a,b) {
     return a + b;
@@ -40,9 +23,76 @@ function divide(a,b) {
     return b === 0 ? 'undefined' : a/b;
 }
 //#endregion
-function operate(operator,a,b) {
-    operator(a,b);
+function operate(operator) {
+    if(totalValue.textContent === ''){
+        intTotalValue = +currentNumber.textContent;
+        updateNumbers(operator);
+        return;
+    }
+    switch (operator) {
+        case '+':
+            intTotalValue = add(intTotalValue,+currentNumber.textContent);
+            updateNumbers(operator);
+            break;
+        case '-':
+            intTotalValue = subtract(intTotalValue,+currentNumber.textContent);
+            updateNumbers(operator);
+            break;
+        case '/':
+            intTotalValue = divide(intTotalValue,+currentNumber.textContent);
+            updateNumbers(operator);
+            break;
+        case '*':
+            intTotalValue = multiply(intTotalValue,+currentNumber.textContent);
+            updateNumbers(operator);
+            break;
+        case '=':
+            if(totalValue.textContent === '') return;
+            isFinalNumber = true;
+            operate(totalValue.textContent[totalValue.textContent.length - 2]);
+            break;
+        default:
+            break;
+    }
+}
+
+function updateNumbers(operation){
+    if(isFinalNumber){
+        totalValue.textContent = totalValue.textContent + currentNumber.textContent + ' =';
+        currentNumber.textContent = intTotalValue.toString();
+        totalValuePreview = true;
+        return;
+    }
+
+    if(totalValue.textContent === ''){
+        totalValue.textContent = currentNumber.textContent + ` ${operation} `;
+    } else{
+    totalValue.textContent = totalValue.textContent + currentNumber.textContent + ` ${operation} `;
+    }
+    currentNumber.textContent = intTotalValue.toString();
+    totalValuePreview = true;
 }
 function insertNumber(e){
+    if(currentNumber.textContent === '0' && e.target.textContent === '0') return;
+    if(isFinalNumber) {
+        totalValue.textContent = ''
+        intTotalValue = 0;
+        isFinalNumber = false;
+    }
+    if(totalValuePreview) {
+        currentNumber.textContent = '';
+        totalValuePreview = false;
+    }
     currentNumber.textContent += e.target.textContent;
+}
+function changeSign() {
+    if(currentNumber.textContent[0] != '-'){
+        currentNumber.textContent = '-' + currentNumber.textContent;
+    } else{
+        currentNumber.textContent = currentNumber.textContent.split('-')[1];
+    }
+}
+function addDecimal() {
+    if(currentNumber.textContent.includes('.')) return;
+    currentNumber.textContent += '.';
 }
